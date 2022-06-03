54e8dbd37d85ec08fedfbe5d9cc428083df698ed
Radityo Fajar <54077977+Radityofajar@users.noreply.github.com>
2022-06-03 13:24:35 +0900

    Update model_init_2.py

--- model_init_2.py
+++ model_init_2.py
@@ -2,7 +2,7 @@ from joblib import dump
 import numpy as np
 import pandas as pd
 from pyod.models.hbos import HBOS
-#from pyod.models.mcd import MCD
+from pyod.models.mcd import MCD
 import time
 from sklearn.preprocessing import StandardScaler
 
@@ -16,9 +16,7 @@ df_temp1 = np.array([data_frame['temperature1']]).T
 df_temp2 = np.array([data_frame['temperature2']]).T
 df_temp3 = np.array([data_frame['temperature3']]).T
 df_waterlevel = np.array([data_frame['waterlevel']]).T
-df_waterleak = np.array([data_frame['leakage']]).T
-df_fire = np.array([data_frame['fire']]).T
-df_door = np.array([data_frame['isclosed']]).T
+
 
 sc_hum1 = StandardScaler().fit_transform(df_hum1)
 sc_hum2 = StandardScaler().fit_transform(df_hum2)
@@ -26,9 +24,7 @@ sc_temp1 = StandardScaler().fit_transform(df_temp1)
 sc_temp2 = StandardScaler().fit_transform(df_temp2)
 sc_temp3 = StandardScaler().fit_transform(df_temp3)
 sc_waterlevel = StandardScaler().fit_transform(df_waterlevel)
-sc_waterleak = StandardScaler().fit_transform(df_waterleak)
-sc_fire = StandardScaler().fit_transform(df_fire)
-sc_door = StandardScaler().fit_transform(df_door)
+
 
 model_hum1 = HBOS(contamination=outliers_fraction)
 model_hum2 = HBOS(contamination=outliers_fraction)
@@ -46,10 +42,8 @@ model_temp1 = MCD(contamination=outliers_fraction,random_state=42)
 model_temp2 = MCD(contamination=outliers_fraction,random_state=42)
 model_temp3 = MCD(contamination=outliers_fraction,random_state=42)
 model_waterlevel = MCD(contamination=outliers_fraction,random_state=42)
-model_waterleak = MCD(contamination=outliers_fraction,random_state=42)
-model_fire = MCD(contamination=outliers_fraction,random_state=42)
-model_door = MCD(contamination=outliers_fraction,random_state=42)
 '''
+
 t0 = time.time()
 model_hum1.fit(sc_hum1)
 model_hum2.fit(sc_hum2)
@@ -57,9 +51,7 @@ model_temp1.fit(sc_temp1)
 model_temp2.fit(sc_temp2)
 model_temp3.fit(sc_temp3)
 model_waterlevel.fit(sc_waterlevel)
-model_waterleak.fit(sc_waterleak)
-model_fire.fit(sc_fire)
-model_door.fit(sc_door)
+
 t1 = time.time()
 print(t1-t0)
 
@@ -69,16 +61,17 @@ dump(model_temp1, 'model\HBOS_model_temp1.joblib')
 dump(model_temp2, 'model\HBOS_model_temp2.joblib')
 dump(model_temp3, 'model\HBOS_model_temp3.joblib')
 dump(model_waterlevel, 'model\HBOS_model_waterlevel.joblib')
-dump(model_waterleak, 'model\HBOS_model_waterleak.joblib')
-dump(model_fire, 'model\HBOS_model_fire.joblib')
-dump(model_door, 'model\HBOS_model_door.joblib')
-
+'''
+dump(model_hum1, 'model\MCD_model_hum1.joblib')
+dump(model_hum2, 'model\MCD_model_hum2.joblib')
+dump(model_temp1, 'model\MCD_model_temp1.joblib')
+dump(model_temp2, 'model\MCD_model_temp2.joblib')
+dump(model_temp3, 'model\MCD_model_temp3.joblib')
+dump(model_waterlevel, 'model\MCD_model_waterlevel.joblib')
+'''
 dump(sc_hum1, 'SC\std_scaler_hum1.bin')
 dump(sc_hum2, 'SC\std_scaler_hum2.bin')
 dump(sc_temp1, 'SC\std_scaler_temp1.bin')
 dump(sc_temp2, 'SC\std_scaler_temp2.bin')
 dump(sc_temp3, 'SC\std_scaler_temp3.bin')
 dump(sc_waterlevel, 'SC\std_scaler_waterlevel.bin')
-dump(sc_waterleak, 'SC\std_scaler_waterleak.bin')
-dump(sc_fire, 'SC\std_scaler_fire.bin')
-dump(sc_door, 'SC\std_scaler_door.bin')
