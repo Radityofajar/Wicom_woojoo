2beabf592c93466b9c45e175d5d86df4dd545a7f
Radityo Fajar <54077977+Radityofajar@users.noreply.github.com>
2022-07-14 18:19:58 +0900

    fix typo

--- wicom_postproc_thtd.py
+++ wicom_postproc_thtd.py
@@ -211,8 +211,8 @@ def post_process(rawdata):
             
             if anomaly_score_hum_cal <= -0.15:
                 anomaly_threshVal1 = -0.15
-            elif anomaly_score_hum_cal >= 0.01:
-                anomaly_threshVal1 = 0.01
+            elif anomaly_score_hum_cal >= 0.0:
+                anomaly_threshVal1 = 0.0
             else:
                 anomaly_threshVal1 = anomaly_score_hum_cal
 
@@ -243,11 +243,11 @@ def post_process(rawdata):
 
         #print the value in the terminal
         print('temp value: '+str(sensor_temp[0]))
-        print('temp score: '+str(anomaly_score_temp))
-        print('temp threshold: '+str(anomaly_threshVal0))
+        print('temp score: '+str(float(anomaly_score_temp)))
+        print('temp threshold: '+str(float(anomaly_threshVal0)))
         print('hum value: '+str(sensor_hum[0]))
-        print('hum score: '+str(anomaly_score_hum))
-        print('hum threshold: '+str(anomaly_threshVal1))
+        print('hum score: '+str(float(anomaly_score_hum)))
+        print('hum threshold: '+str(float(anomaly_threshVal1)))
 
         #clustering between normal & abnormal
         #temperature sensor
@@ -290,6 +290,8 @@ def post_process(rawdata):
 
         nid_library_2[score_nid] = np.append(nid_library_2[score_nid],float(anomaly_score_hum))
         nid_library_2[status_nid] = np.append(nid_library_2[status_nid],sensor_hum_status)
+
+        print('window_size: ' + str(len(nid_library[sensor_nid])))
 
         #store the data in order to send it back to IoT.own
         changedata = {}
